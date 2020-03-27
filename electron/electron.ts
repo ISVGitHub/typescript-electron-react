441490c45c6c29fd94d1fd3c68db0fe07044f798
ISV <548464+ISVGitHub@users.noreply.github.com>
2020-03-27 18:14:00 +0600

    Delete minimal system tray application

--- electron/electron.ts
+++ electron/electron.ts
@@ -1,6 +1,5 @@
 import { app, BrowserWindow } from "electron";
 import * as path from "path";
-import {InitTray} from './trayWindow';
 
 let mainWindow: Electron.BrowserWindow | null;
 
@@ -32,10 +31,7 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", () => {
-  createWindow();
-  InitTray();
-});
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
